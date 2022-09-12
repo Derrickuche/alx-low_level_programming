6c6ba4f67d6f93bbdcc1cdcd2a30998aa9c73a0d
Derrickuche <uchechukwuderrick@gmail.com>
2022-09-11 20:41:52 -0700

    print-f

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,10 @@
-include <stdio.h>
 #include <stdio.h>
 
 /**
 * main - print the string in the put function
 *
 * Description: using the main function
-* this program prints "programming is likebuilding a multilingual puzzle
-* Return: 0
+* this prigram prints "programming is like building a multillingual puzzle
 */
 int main(void)
 {
